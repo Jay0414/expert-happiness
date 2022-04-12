382a57702b8803016dda36460f582bc5ba1b4e3a
Jay0414 <17367943832@163.com>
2022-04-12 19:05:21 +0800

    3

---
+++ 333.py
@@ -0,0 +1,31 @@
+# a = 60
+# b = 15
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+
+a = 99
+b = 10.234
+c = "hello word"
+d = True
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+# 整数转成浮点数
+print(float(a))
+# 浮点型转换成字符串
+print(str(b))
+# 字符串转换成布尔型
+print(bool(c))
+# 布尔型转换成整数
+print(int(d))
+# 整数转换成对应的字符
+print(chr(98))
+# 将字符转成整数 (Python中字符和字符串表示法相同)
+print(ord("a"))
+
+
